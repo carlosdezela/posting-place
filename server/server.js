dc97e82530c69008eed8c83a1269a60b484c8bc5
Carlos De Zela <dezelaperezcarlos@gmail.com>
2024-03-29 21:38:44 -0500

    Recepción de los mensajes.

--- server/server.js
+++ server/server.js
@@ -1,26 +1,35 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json());
+
+let data = {
+  "messages": [
+    {
+      name: "Bob",
+      text: "Buenos días mundo!",
+      date: "29/3/2024"
+    },
+    {
+      name: "Alice",
+      text: "Buenas noches mundo!",
+      date: "29/3/2024"
+    }
+  ]
+}
+
 app.get('/api', (req, res) => {
-  res.json({
-    "messages": [
-      {
-        name: "Carlos De Zela",
-        text: "Hola mundo!",
-        date: "29/03/2024"
-      },
-      {
-        name: "Bob",
-        text: "Buenos días mundo!",
-        date: "29/03/2024"
-      },
-      {
-        name: "Alice",
-        text: "Buenas noches mundo!",
-        date: "29/03/2024"
-      }
-    ]
-  })
+  res.json(data)
+})
+
+app.post('/msg', (req, res) => {
+  let newMsg = req.body;
+  data.messages.push({
+    name: "Anónimo",
+    text: newMsg.content,
+    date: newMsg.date
+  });
+  console.log(data)
 })
 
 app.listen(4444, () => {
